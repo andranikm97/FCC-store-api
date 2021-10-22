3579123c3d095bb92ea0ebedeee4c316aa3ab68b
Andranik Markaryan <andranik.mrk@gmail.com>
2021-10-23 00:58:48 +0300

    Boot up basic server
    Modified the 'app.js' server file. Assigned 'not foun' and 'error-handling' middleware to the app. Made a simple route to check for server functionality.

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-console.log('04 Store API')
+require('dotenv').config();
+// Async errors
+
+const express = require('express');
+const app = express();
+
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('<h1>Store API</h1><a href="/api/v1/products">Products route</a>');
+});
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const startApp = async () => {
+  try {
+    // connectToDB with await
+    app.listen(port, () => {
+      console.log(`Listening at http://localhost:${port}`);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+startApp();
