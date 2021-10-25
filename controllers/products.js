a416aedce2bbdece4c0a73d7ad1a87b45d9c2440
Andranik Markaryan <andranik.mrk@gmail.com>
2021-10-25 16:08:18 +0300

    Implement sort functionality
    Upon receiving request, if a 'sort' query was receieved, it is passed to an if-else statement that decides how to sort the input and send it back to client. The await method is moved to the very end of the router controller.

--- controllers/products.js
+++ controllers/products.js
@@ -6,7 +6,7 @@ const getAllProductsStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name } = req.query;
+  const { featured, company, name, sort } = req.query;
   const queryObject = {};
 
   if (featured) {
@@ -21,8 +21,16 @@ const getAllProducts = async (req, res) => {
     queryObject.name = { $regex: name, $options: 'i' };
   }
 
-  console.log(queryObject);
-  const products = await Product.find(queryObject);
+  let result = Product.find(queryObject);
+
+  if (sort) {
+    const sortList = sort.split(',').join(' ');
+    result = result.sort(sortList);
+  } else {
+    result = result.sort('-createdAt');
+  }
+
+  const products = await result;
   res.status(200).json({ data: products, nbHits: products.length });
 };
 
