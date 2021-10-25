e963eb28e0aff6d1dd9f23b42c75b25a8e21dfdb
Andranik Markaryan <andranik.mrk@gmail.com>
2021-10-25 16:29:35 +0300

    Implement pagination functionality
    With help of native MongoDB queries, implemented pagination that is based on the 'skip' and 'limit' methods.

--- controllers/products.js
+++ controllers/products.js
@@ -6,8 +6,10 @@ const getAllProductsStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort, fields, limit } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
   const queryObject = {};
+
+  // Query
 
   if (featured) {
     queryObject.featured = featured === 'true';
@@ -23,6 +25,7 @@ const getAllProducts = async (req, res) => {
 
   let result = Product.find(queryObject);
 
+  // SORT
   if (sort) {
     const sortList = sort.split(',').join(' ');
     result = result.sort(sortList);
@@ -30,15 +33,17 @@ const getAllProducts = async (req, res) => {
     result = result.sort('-createdAt');
   }
 
+  // SELECTED FIELDS
   if (fields) {
     const fieldsList = fields.split(',').join(' ');
     result = result.select(fieldsList);
   }
 
-  if (limit) {
-    // console.log(typeof limit);
-    result = result.limit(Number(limit));
-  }
+  // PAGINATION
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+  result = result.skip(skip).limit(limit);
 
   const products = await result;
   res.status(200).json({ data: products, nbHits: products.length });
