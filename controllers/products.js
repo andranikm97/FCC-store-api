0b05f10e20a8fce8a81fde10db18658ac07eb24f
Andranik Markaryan <andranik.mrk@gmail.com>
2021-10-25 15:09:20 +0300

    Setup 'get all products' static route and fix bug
    Implemented the router controller for the static route. Also, fixed key name in mongoose scheme from "feature" to "featured".

--- controllers/products.js
+++ controllers/products.js
@@ -1,8 +1,8 @@
 const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
-  const products = Product.find({});
-  res.status(200).json({ data: products });
+  const products = await Product.find({ featured: true });
+  res.status(200).json({ data: products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
