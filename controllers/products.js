5f4ea7e28d260de3c6bc6dacc4139c9651a12ab3
Andranik Markaryan <andranik.mrk@gmail.com>
2021-10-25 22:40:01 +0300

    Add numeric filtering
    Implemented a numeric filtering condition that uses regular experssions
    object to feed the queryObject object. A very handy implementation.

--- controllers/products.js
+++ controllers/products.js
@@ -6,7 +6,7 @@ const getAllProductsStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort, fields } = req.query;
+  const { featured, company, name, sort, fields, numericFilters } = req.query;
   const queryObject = {};
 
   // Query
@@ -21,6 +21,29 @@ const getAllProducts = async (req, res) => {
 
   if (name) {
     queryObject.name = { $regex: name, $options: 'i' };
+  }
+
+  if (numericFilters) {
+    const operatorMap = {
+      '>': '$gt',
+      '>=': '$gte',
+      '=': '$eq',
+      '<': '$lt',
+      '<=': '$lte',
+    };
+
+    const regEx = /\b(<|>|>=|=|<=)\b/g;
+    let filters = numericFilters.replace(
+      regEx,
+      (match) => `-${operatorMap[match]}-`,
+    );
+    const options = ['price', 'rating'];
+    filters = filters.split(',').forEach((item) => {
+      const [option, operation, amount] = item.split('-');
+      if (options.includes(option)) {
+        queryObject[option] = { [operation]: Number(amount) };
+      }
+    });
   }
 
   let result = Product.find(queryObject);
